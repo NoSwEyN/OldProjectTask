59c565b0cd2cb0b6e4c46eb804bcd32f97c4844e
Debis <mr.lubenetes@mail.ru>
2025-08-01 19:17:49 +0700

    Исправил ошибку в коде

--- internal/taskService/orm.go
+++ internal/taskService/orm.go
@@ -5,10 +5,3 @@ type Task struct {
 	Task   string `json:"task"`
 	UserID int    `gorm:"not null" json:"user_id"`
 }
-
-type User struct {
-	ID       int    `gorm:"primaryKey;autoIncrement" json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Tasks    []Task `gorm:"foreignKey:UserID; references:ID" json:"tasks"`
-}
